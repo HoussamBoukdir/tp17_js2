5364c1edc92e85bdeb0e67ed8145e0b403550e11
HoussamBoukdir <boukdir.houssam166@gmail.com>
2025-11-24 00:36:23 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+let val1=2>3? "vrai":"faux";
+console.log(val1);
+
+let nom="Achraf";
+let salutation =nom? `Bonjour ${nom}`:`Bonjour invité`;
+console.log(salutation);
+
+let age=45;
+let etat=age<50?"jeune":"vieux";
+console.log(etat);
+
+let etat1=age<18?"jeune":age<=65?"d'âge moyen":"vieux";
+console.log(etat1);
+
